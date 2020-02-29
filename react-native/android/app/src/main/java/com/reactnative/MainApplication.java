bf156cc337b759da97cdaed7b68f10e1f36cae89
felipehjcosta <felipe.h.j.costa@gmail.com>
2020-02-28 23:36:38 -0300

    [react-native sample] Measure total memory of the current process on Android

--- react-native/android/app/src/main/java/com/reactnative/MainApplication.java
+++ react-native/android/app/src/main/java/com/reactnative/MainApplication.java
@@ -61,6 +61,7 @@ public class MainApplication extends Application implements ReactApplication {
   @Override
   public void onCreate() {
     super.onCreate();
+    new RamUsageCollector();
     SoLoader.init(this, /* native exopackage */ false);
   }
 }
