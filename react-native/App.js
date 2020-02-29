904cb7d312f9534e6eef4ad6b33d8c1ebea27a40
felipehjcosta <felipe.h.j.costa@gmail.com>
2020-02-28 23:33:34 -0300

    [react-native sample] Add timer sample

--- react-native/App.js
+++ react-native/App.js
@@ -1,19 +1,53 @@
 import React from 'react';
 import { StyleSheet, Text, View } from 'react-native';
+import { sprintf } from 'sprintf-js';
 
 export default function App() {
   return (
     <View style={styles.container}>
-      <Text>Open up App.js to start working on your app!</Text>
+      <Timer />
     </View>
   );
+}
+
+class Timer extends React.Component {
+  constructor(props) {
+    super(props);
+    this.state = {
+      milliseconds: 0,
+      seconds: 0,
+      minutes: 0,
+    }
+
+    let startTime = new Date().getTime();
+    setInterval(() => {
+      let timeDifference = new Date().getTime() - startTime;
+      let seconds = timeDifference / 1000;
+      let minutes = seconds / 60;
+      let leftoverSeconds = seconds % 60;
+      let leftoverMillis = timeDifference % 1000 / 10;
+      this.setState({
+        milliseconds: leftoverMillis,
+        seconds: leftoverSeconds,
+        minutes: minutes,
+      });
+    }, 10);
+  }
+
+  render() {
+    let { milliseconds, seconds, minutes } = this.state;
+    let time = sprintf("%02d:%02d:%2d", minutes, seconds, milliseconds);
+    return (
+        <Text>{time}</Text>
+    )
+  }
 }
 
 const styles = StyleSheet.create({
   container: {
     flex: 1,
-    backgroundColor: '#fff',
-    alignItems: 'center',
     justifyContent: 'center',
-  },
+    alignItems: 'center',
+    backgroundColor: '#F5FCFF',
+  }
 });
