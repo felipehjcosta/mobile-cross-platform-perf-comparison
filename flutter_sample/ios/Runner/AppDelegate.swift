63d2453e9d6c02cd0926b5eb3342e95ee93d66dc
felipehjcosta <felipe.h.j.costa@gmail.com>
2020-03-03 09:53:25 -0300

    [flutter sample] Measure total memory of the current process on iOS

--- flutter_sample/ios/Runner/AppDelegate.swift
+++ flutter_sample/ios/Runner/AppDelegate.swift
@@ -7,6 +7,11 @@ import Flutter
     _ application: UIApplication,
     didFinishLaunchingWithOptions launchOptions: [UIApplication.LaunchOptionsKey: Any]?
   ) -> Bool {
+    
+    Timer.scheduledTimer(withTimeInterval: 1.0, repeats: true) { _ in
+        print("Total memory of the current process in: \(Memory.formattedMemoryFootprint())")
+    }
+    
     GeneratedPluginRegistrant.register(with: self)
     return super.application(application, didFinishLaunchingWithOptions: launchOptions)
   }
---
+++ flutter_sample/ios/Runner/Memory.swift
@@ -0,0 +1,44 @@
+//
+//  Memory.swift
+//  Runner
+//
+//  Created by Felipe Costa on 02/03/20.
+//  Copyright © 2020 The Chromium Authors. All rights reserved.
+//
+
+import Foundation
+
+class Memory: NSObject {
+
+    // From Quinn the Eskimo at Apple.
+    // https://forums.developer.apple.com/thread/105088#357415
+
+    class func memoryFootprint() -> Float? {
+        // The `TASK_VM_INFO_COUNT` and `TASK_VM_INFO_REV1_COUNT` macros are too
+        // complex for the Swift C importer, so we have to define them ourselves.
+        let TASK_VM_INFO_COUNT = mach_msg_type_number_t(MemoryLayout<task_vm_info_data_t>.size / MemoryLayout<integer_t>.size)
+        let TASK_VM_INFO_REV1_COUNT = mach_msg_type_number_t(MemoryLayout.offset(of: \task_vm_info_data_t.min_address)! / MemoryLayout<integer_t>.size)
+        var info = task_vm_info_data_t()
+        var count = TASK_VM_INFO_COUNT
+        let kr = withUnsafeMutablePointer(to: &info) { infoPtr in
+            infoPtr.withMemoryRebound(to: integer_t.self, capacity: Int(count)) { intPtr in
+                task_info(mach_task_self_, task_flavor_t(TASK_VM_INFO), intPtr, &count)
+            }
+        }
+        guard
+            kr == KERN_SUCCESS,
+            count >= TASK_VM_INFO_REV1_COUNT
+            else { return nil }
+
+        let usedBytes = Float(info.phys_footprint)
+        return usedBytes
+    }
+
+    class func formattedMemoryFootprint() -> String
+    {
+        let usedBytes: UInt64? = UInt64(self.memoryFootprint() ?? 0)
+        let usedMB = Double(usedBytes ?? 0) / 1024 / 1024
+        let usedMBAsString: String = "\(usedMB) MB"
+        return usedMBAsString
+     }
+}
